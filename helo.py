11c7db5d8b453aaf5f506ff26df87d73876c36d3
DucAnh <jokerducanh@gmail.com>
2026-01-17 16:13:59 +0700

    xoa

--- helo.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print("--- BẮT ĐẦU CHƯƠNG TRÌNH ---")
-for i in range(1, 6):
-    print(f"Đang xử lý lần thứ {i}...")
-    time.sleep(1) 
-
-print("--- KẾT THÚC VÀ ĐẨY LÊN GITHUB THÀNH CÔNG! ---")
